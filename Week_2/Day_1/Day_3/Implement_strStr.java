eacad20af79be044618a37b718f35941441f2674
Pallavi Verma <vpallavi083@gmail.com>
2026-06-10 22:24:43 +0530

    Add files via upload

---
+++ Week_2/Day_1/Day_3/Implement_strStr.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int a = haystack.length();
+        int b = needle.length();
+
+        for(int i = 0; i <= a-b; i++ ){
+            if(haystack.substring(i,i+b).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
